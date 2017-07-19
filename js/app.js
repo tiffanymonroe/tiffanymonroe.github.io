67f5d052aaacc2f92d44b76a234d01abe29f53bf
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-19 13:41:23 -0700

    category class and first set of categories set

--- js/app.js
+++ js/app.js
@@ -7,42 +7,82 @@ $('button').on('click', ()=>{
   $('.row-2').css('display', 'flex')
 })
 
+
 //local scope
 const game = {
   players: [],
   rounds: 0,
-  score: 0,
-  getName(){
-      $('input').on('keypress', (e)=>{
-      console.log('the key had been pressed');
-      $('button').on('click', ()=>{
-      $player1 = $('#player1 input').val();
-      $('#player1-info').append('<div>').text($player1);
-      })
-  })
-  }
-
+  score: 0
 }
 
-game.getName();
-console.log(game.players);
 
-class Player {
-  constructor (name, score, roundsWon) {
-    this.name = '';
-    this.score = 0;
-    this.roundsWon = 0;
+//Categories //
+
+class Category {
+  constructor(name, questions, choices, answers){
+    this.name = name;
+    this.questions = [];
+    this.choices = [];
+    this.answers = [];
   }
-
+  setQuestion(newQuestion){
+    this.questions = this.questions.push(newQuestion);
+  }
+  setChoices(newChoices){
+    this.choices = this.choices.push(newChoices);
+  }
+  setAnswer(newAnswer){
+    this.answers = this.answers.push(newAnswer);
+  }
+  // setCorrectAnswer(){
+  //   if statement
+  // }
 }
 
-let player1 = new Player;
-console.log(player1);
-let player2 = new Player;
-console.log(player2);
+//*****************************************//
+const disney = new Category ('Disney');
+//question 1
+disney.setQuestion('What date did Disneyland open?');
+disney.setAnswer('July 17, 1955');
+disney.setChoices('December 5, 1950', 'July 17, 1955', 'June 11, 1955');
+
+//
+console.log(disney);
+
+//*****************************************//
+const buffyverse = new Category ('Buffyverse');
+//question 1
+buffyverse.setQuestion("What dimension is Lorne from");
+buffyverse.setAnswer("Pylea");
+buffyverse.setChoices("Oden-Tal", "Arashmaharr", "Pylea");
+//
+
+console.log(buffyverse);
 
 
+//*****************************************//
+const starwars = new Category ('Star Wars');
+starwars.setQuestion("What is the title of Darth Vader's theme music?");
+starwars.setAnswer("The Imperial March");
+starwars.setChoices("The Imperial March", "The Empire", "Darth Vader Theme Music");
 
+
+// leave until last
+// getName = ()=>{
+//     $('input').on('click', ()=>{
+//       $('button').on('click', ()=>{
+//         $player1 = $('#player1 input').val();
+//         $player2 = $('#player2 input').val();
+//         game.players.push($player1, $player2);
+//         $('#player1-info').append('<div>')
+//         $('#player2-info').append('<div>')
+//     })
+// })
+// }
+//
+// getName();
+//
+// console.log(game.players);
 
 
 }) //end window onload
