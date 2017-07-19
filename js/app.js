30f8b25a59f7fdf27d1b2ec25e164714008f3b24
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-18 22:15:01 -0700

    categories appear on button click

--- js/app.js
+++ js/app.js
@@ -1,10 +1,39 @@
 $(()=>{
 
 //global scope
-const $categories = ['Disney', 'Harry Potter', 'Lord of the Rings', 'Star Trek', 'Star Wars', 'Buffyverse', 'Superheroes'];
+const categories = ['Disney', 'Harry Potter', 'Lord of the Rings', 'Star Trek', 'Star Wars', 'Buffyverse', 'Superheroes'];
 
 
+
+const getName = ()=>{
+  $('input').on('keypress', (e)=>{
+  console.log('the key had been pressed');
+  $('button').on('click', ()=>{
+  $name = $(e.currentTarget).val();
+  $('.name').append($name).text($name);
+  })
+})
+}
 //local scope
+
+
+class Player {
+  constructor (name, score, roundsWon) {
+    this.name = getName();
+    this.score = 0;
+    this.roundsWon = 0;
+  }
+
+}
+
+let player1 = new Player;
+console.log(player1);
+let player2 = new Player;
+console.log(player2);
+
+$('button').on('click', ()=>{
+  $('.row-2').css('display', 'flex')
+})
 
 
 
