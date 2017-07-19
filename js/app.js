2ab40ec952b2e8b57c385a4aa092ae117d033436
Tiffany Monroe <hello@tiffanymonroe.com>
2017-07-18 20:15:06 -0700

    revised setup

--- js/app.js
+++ js/app.js
@@ -1,6 +1,10 @@
 $(()=>{
 
+//global scope
+const $categories = ['Disney', 'Harry Potter', 'Lord of the Rings', 'Star Trek', 'Star Wars', 'Buffyverse', 'Superheroes'];
 
+
+//local scope
 
 
 }) //end window onload
