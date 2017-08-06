25d1ec859d05094f9e826b5fc132a8fc1871a027
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-06 14:43:25 -0700

    reorganizing files

--- project_two_v1/models/user.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = new mongoose.Schema({
-  email: String,
-  password: String,
-})
-
-
-module.exports = mongoose.model('userSchema', userSchema)
