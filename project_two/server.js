c85782340a8d42df00ba47d3e1d2f07f7543cb27
Tiffany Monroe <hello@tiffanymonroe.com>
2017-08-06 12:07:33 -0700

    project 2 redo: server, index route connected and working

--- project_two/server.js
+++ project_two/server.js
@@ -1,36 +1,31 @@
-//Dependancies
-
+//dependencies
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const methodOverride = require('method-override');
-const Student = require('./controllers/students.js');
 
-//Middleware
+//middleware
 app.use(methodOverride('_method'));
-app.use(bodyParser.urlencoded({extended:false}));
-app.use(express.static ('public') );
+app.use(bodyParser.urlencoded({extended: false}));
 
 
-//controllers
-const studentsController = require('./controllers/students.js');
-app.use('/students', studentsController);
-
-//Index Route
+//index route
 app.get('/', (req, res)=>{
   res.render('index.ejs')
 });
 
 
-//Connections
-
+//mongoose
 mongoose.connect('mongodb://localhost:27017/blog');
 
-mongoose.connection.once('open', () => {
-  console.log('connected to mongo');
-});
 
-app.listen(3000, ()=> {
+mongoose.connection.once('open', ()=>{
+  console.log('mongo is in the house');
+})
+
+//listener
+
+app.listen(3000, ()=>{
   console.log("project two, woo hoo!");
 })
